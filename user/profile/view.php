6e424327e48a3e4548c96d47e3c6853ff8d70988
ArjunlalDotwibe <arjund@twibe@3>
2025-06-17 19:48:16 +0530

    up

--- user/profile/view.php
+++ user/profile/view.php
@@ -43,6 +43,7 @@ if(!empty($userDetails['profile_pic'])){
 
 <link rel='stylesheet' href='<?=SITEURL?>assets/css/all.min.css?v=<?=time()?>' type='text/css' media='all' />
 <link rel='stylesheet' href='<?=SITEURL?>assets/css/themes.css?v=<?=time()?>' type='text/css' media='all' />
+<link rel='stylesheet' href='<?=SITEURL?>assets/css/stylesheet.css?v=<?=time()?>' type='text/css' media='all' />
 
 </head>
 
