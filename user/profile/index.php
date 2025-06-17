266c5d55cd99708424efadccc6e46da2b940662d
ArjunlalDotwibe <arjund@twibe@3>
2025-06-17 18:19:19 +0530

    up

--- user/profile/index.php
+++ user/profile/index.php
@@ -80,7 +80,7 @@ if(!empty($userDetails['profile_pic'])){
 
     $followed_ids = [];
     while ($row = $result->fetch_assoc()) {
-        $followed_ids[] = $row['unique_model_id'];
+        $followed_ids[] = 'test';
     }
 
     echo "<pre>Followed IDs:\n";
