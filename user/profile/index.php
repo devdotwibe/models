f1a57a9dbe16e55825af181a32eec3e632051300
ArjunlalDotwibe <arjund@twibe@3>
2025-06-17 18:47:02 +0530

    up

--- user/profile/index.php
+++ user/profile/index.php
@@ -108,6 +108,10 @@ if (!empty($followed_model_unique_ids)) {
 // echo print_r($followed_user_ids);
 // Step 3: Fetch posts
 if (!empty($followed_user_ids)) {
+
+
+echo print_r('test');
+
     $placeholders = implode(',', array_fill(0, count($followed_user_ids), '?'));
     $types = str_repeat('i', count($followed_user_ids));
     $sql = "SELECT * FROM live_posts WHERE post_author IN ($placeholders) ORDER BY created_at DESC";
