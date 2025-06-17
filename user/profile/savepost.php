a5b357d7a93026bc21aca4dff5e9d93752efdf55
ArjunlalDotwibe <arjund@twibe@3>
2025-06-17 20:51:26 +0530

    up

--- user/profile/savepost.php
+++ user/profile/savepost.php
@@ -24,7 +24,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
     }
 
-    if ($con->connect_error) {
+    if (!$con->connect_error) {
         die("Connection failed: " . $con->connect_error);
     }
 
