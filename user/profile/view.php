557630ac5a9fbe0594717faa9d6624dcabbdc0de
ArjunlalDotwibe <arjund@twibe@3>
2025-06-17 20:03:29 +0530

    up

--- user/profile/view.php
+++ user/profile/view.php
@@ -40,7 +40,7 @@ if(!empty($userDetails['profile_pic'])){
 <script src="https://cdn.tailwindcss.com"></script>
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&family=Playfair+Display:wght@400;500;600;700;800&display=swap" rel="stylesheet">
 
-<link rel='stylesheet' href='<?=SITEURL?>assets/css/stylesheet.css?v=<?=time()?>' type='text/css' media='all' />
+<link rel='stylesheet' href='<?=SITEURL?>assets/css/profile.css?v=<?=time()?>' type='text/css' media='all' />
 
 </head>
 
