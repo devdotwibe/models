64f6179807a15db9e9c550d7dc16be60193da276
ArjunlalDotwibe <arjund@twibe@3>
2025-06-18 20:20:38 +0530

    up

--- includes/profile_header_index.php
+++ includes/profile_header_index.php
@@ -86,7 +86,7 @@
       Support
     </div>
 
-    <div class="menu-item" onclick="logout()">
+    <div class="menu-item" onclick="window.location.href='<?= SITEURL ?>logout.php'">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
       </svg>
