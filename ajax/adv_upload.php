2d1bff41df3c4e281a33f33d135c5db83e57fcb4
coderdotwibe <coder@dotwibe.com>
2025-06-23 15:34:23 +0530

    test

--- ajax/adv_upload.php
+++ ajax/adv_upload.php
@@ -1,7 +1,7 @@
 <?php 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
 var_dump($_FILES); exit;
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['uploaded_file'])) {
 	
