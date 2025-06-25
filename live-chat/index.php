60e875c0129327c6228aec12c3b4a0f5db8d2583
ArjunlalDotwibe <arjund@twibe@3>
2025-06-25 11:01:13 +0530

    up

--- live-chat/index.php
+++ live-chat/index.php
@@ -9,11 +9,10 @@ if(isset($_SESSION["log_user_id"])){
 	$usern = $_SESSION["log_user"];
 
 	$userDetails = get_data('model_user',array('id'=>$_SESSION["log_user_id"]),true);
-	if($userDetails){}
-	else{
-
+	if($userDetails){
     die('ooo');
-    
+  }
+	else{
 		echo '<script>window.location.href="'.SITEURL.'login.php"</script>';
 		die;
 	}
