b5d7807bcdbe2e03f41dfc08306e10bfb6b4ffa6
ArjunlalDotwibe <arjund@twibe@3>
2025-06-26 16:50:32 +0530

    up

--- webrtc-broadcast/start-server.php
+++ webrtc-broadcast/start-server.php
@@ -1,5 +1,10 @@
 <?php
+// Get current directory
+$currentDir = __DIR__;
+echo "Current directory: $currentDir\n";
 
-$command = 'nohup php /var/www/models.staging3.dotwibe.com/html/webrtc-broadcast/server.php > /dev/null 2>&1 &';
-exec($command);
-echo "WebSocket server started.";
+// Build full path to server.php
+$command = "nohup php $currentDir/server.php > /dev/null 2>&1 &";
+// exec($command);
+
+echo "WebSocket server started." .$command;
