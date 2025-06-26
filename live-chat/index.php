75f349921696a68612f57bb9564b10fbd32cb3a9
ArjunlalDotwibe <arjund@twibe@3>
2025-06-26 14:58:56 +0530

    up

--- live-chat/index.php
+++ live-chat/index.php
@@ -1005,9 +1005,8 @@ Please wait...
   <script src="http://muazkhan.com:9001/socket.io/socket.io.js"></script> -->
 
   <!-- Recommended CDN version (more stable & secure over HTTPS) -->
-<!-- <script src="https://cdn.jsdelivr.net/npm/rtcmulticonnection@3.7.1/dist/RTCMultiConnection.min.js"></script> -->
+<script src="https://cdn.jsdelivr.net/npm/rtcmulticonnection@3.7.1/dist/RTCMultiConnection.min.js"></script>
 
-<script src="https://cdn.webrtc-experiment.com/RTCMultiConnection.min.js"></script>
 
 <script src="https://cdn.socket.io/4.5.0/socket.io.min.js"></script>
 
