5b6a5e818cf43b2ea5845c85d677ad2684095d72
ArjunlalDotwibe <arjund@twibe@3>
2025-06-30 18:40:10 +0530

    up

--- video-chat/webrtc.js
+++ video-chat/webrtc.js
@@ -4,12 +4,14 @@ let localStream;
 let peerConnection;
 const config = {
   iceServers: [
-    { urls: 'stun:stun.l.google.com:19302' },
-    {
-        urls: 'turn:openrelay.metered.ca:80',
-        username: 'openrelayproject',
-        credential: 'openrelayproject'
-    }
+        {
+            urls: "stun:stun.l.google.com:19302"
+        },
+        {
+            urls: "turn:209.182.232.170:3478",
+            username: "USERNAME",
+            credential: "PASSWORD"
+        }
     ]
 
 };
