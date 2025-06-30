11964a2f1bfe1e19a46b24a12bc3bfd1c51abb6f
ArjunlalDotwibe <arjund@twibe@3>
2025-06-30 19:42:07 +0530

    tes

--- live-chat/index.php
+++ live-chat/index.php
@@ -1150,7 +1150,7 @@ Please wait...
   <!-- <script src="https://unpkg.com/@popperjs/core@2"></script> -->
 
   <script src="<?='https://cdn.jsdelivr.net/npm/rtcmulticonnection@3.7.1/dist/RTCMultiConnection.min.js'?>"></script>
-  <!-- <script src="<?='https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.5.0/socket.io.js'?>"></script>  -->
+  <script src="<?='https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.5.0/socket.io.js'?>"></script> 
   <script src="<?= 'assest/script.js?v=' . time() ?>"></script>
 
   <!-- <script src="<?= 'script.js?v=' . time() ?>"></script> -->
