6dbeb3d3cb33db31643cdcfe61fb2588280c670e
coderdotwibe <coder@dotwibe.com>
2025-07-02 17:04:24 +0530

    Image issue fix

--- advertisement/ajax_item.php
+++ advertisement/ajax_item.php
@@ -20,13 +20,12 @@ if ($all_data) {
                     <div class="col-span-3">
                         <div class="flex items-center space-x-3">
 						<?php 
-						if(!empty($set_data['image']) && file_exists('uploads/banners/'.$set_data['image'])){
+						if(!empty($set_data['image'])){
 							$adv_image = SITEURL.'uploads/banners/'.$set_data['image'];
 						}else if(!empty($set_data['additionalimages'])){
 							$additionalimages = explode('|',$set_data['additionalimages']);
-							//if(file_exists('uploads/banners/'.$additionalimages[0])){
+							//if(file_exists('uploads/banners/'.$additionalimages[0]))
 							$adv_image = SITEURL.'uploads/banners/'.$additionalimages[0];
-							//}else $adv_image = SITEURL.'assets/images/model-gal-no-img.jpg';
 						}else $adv_image = SITEURL.'assets/images/model-gal-no-img.jpg';
 						?>
                             <img src="<?php echo $adv_image; ?>" alt="Ad Preview" class="w-10 h-10 rounded-lg object-cover">
