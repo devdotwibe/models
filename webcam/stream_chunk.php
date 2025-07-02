3edfedac4cddc1704108c87fadd6c32588b72ff4
ArjunlalDotwibe <arjund@twibe@3>
2025-07-02 16:03:11 +0530

    up

--- webcam/stream_chunk.php
+++ webcam/stream_chunk.php
@@ -3,12 +3,12 @@ $index = isset($_GET['index']) ? intval($_GET['index']) : 0;
 $file = __DIR__ . "/uploads/chunks/chunk$index.webm";
 
 if (!file_exists($file)) {
-    http_response_code(204); // No content yet
+    http_response_code(204); // Not ready
     exit;
 }
 
 header("Content-Type: video/webm");
-header("Content-Length: " . filesize($file));
 header("Content-Disposition: inline; filename=\"" . basename($file) . "\"");
+header("Content-Length: " . filesize($file));
 
 readfile($file);
