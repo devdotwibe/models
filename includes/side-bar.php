ac502daf95c9861326734e2bc4924756426e083f
coderdotwibe <coder@dotwibe.com>
2025-07-08 18:11:01 +0530

    wallet

--- includes/side-bar.php
+++ includes/side-bar.php
@@ -69,7 +69,7 @@ if (isset($_SESSION['log_user_id'])) {
       Add Social Links
     </div>
 
-    <div class="menu-item" onclick="navigateTo('#')">
+    <div class="menu-item" onclick="navigateTo('wallet.php')">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" />
       </svg>
