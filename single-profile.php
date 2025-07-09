8c5d41f36a029ea1f3f81444706be35ee05abb0f
ArjunlalDotwibe <arjund@twibe@3>
2025-07-09 16:59:16 +0530

    up

--- single-profile.php
+++ single-profile.php
@@ -194,7 +194,7 @@ if (mysqli_num_rows($res_ap) > 0) {
 									$state_list = DB::query('select name from states where id="'.$rowesdw['state'].'"');
 									$city_list = DB::query('select name from cities where id="'.$rowesdw['city'].'"');
 									if(!empty($country_list) && !empty($country_list[0]['name'])){ ?>
-                                <div class="flex items-center gap-2 text-white/70 mb-2 sm:mb-3 text-sm sm:text-base">
+                                <div class="flex items-center gap-2 text-white/70 mb-2 sm:mb-3 text-sm sm:text-base image-center-profile">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10" r="3"></circle></svg>
                                     
 									<?php echo $city_list[0]['name'].', '.$state_list[0]['name'].', '.$country_list[0]['name']; ?>
