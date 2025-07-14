cfbed94ff38c93605fb61e7b45f757b365c2ac39
ArjunlalDotwibe <arjund@twibe@3>
2025-07-14 14:41:06 +0530

    up

--- single-profile.php
+++ single-profile.php
@@ -444,7 +444,8 @@ if (mysqli_num_rows($res_ap) > 0) {
                             <?php if($uplds['post_type'] =='paid') {?>
 
                                    <div class="token-btn inline-flex items-center justify-center bg-gradient-to-r from-indigo-600 to-indigo-500 text-white px-4 py-1 rounded-full text-sm font-semibold shadow-md cursor-pointer hover:from-indigo-700 hover:to-indigo-600 gap-2">
-                                        <i class="fa fa-database" aria-hidden="true"></i>
+                                      <i class="fas fa-database" aria-hidden="true"></i>
+
                                         <span>20</span>
                                     </div>
 
