3d5d86114cd8075f88fc642ef0cf9fef87a0d964
ArjunlalDotwibe <arjund@twibe@3>
2025-07-14 12:48:01 +0530

    up

--- single-profile.php
+++ single-profile.php
@@ -166,9 +166,7 @@ if (mysqli_num_rows($res_ap) > 0) {
 		}
 	}
 
-    $model_posts = query('select * from live_posts where post_author="'.$model_id.'" Order by id DESC');
-
-    dd($model_posts);
+    $model_posts =  DB::query('select * from live_posts where post_author="'.$model_id.'" Order by id DESC');
 
   ?>
     
