ccd2f0acc5d7c54d0faa11351af8486f0fad905b
ArjunlalDotwibe <arjund@twibe@3>
2025-07-15 20:13:41 +0530

    upo

--- password.php
+++ password.php
@@ -17,15 +17,15 @@ if ($result && mysqli_num_rows($result) > 0) {
         // Hash the password
         $hashed_password = password_hash($plain_password, PASSWORD_DEFAULT);
 
-        var_dump($hashed_password); exit;
-
         $escaped_password = mysqli_real_escape_string($con, $hashed_password);
 
         // Update the hashed password in the database
         $update = "UPDATE model_user SET password = '$escaped_password' WHERE id = $id";
-        mysqli_query($con, $update);
+        if (!mysqli_query($con, $update)) {
+            echo "Failed to update ID $id: " . mysqli_error($con) . "<br>";
+        }
     }
-    echo "Password update completed. tetst";
+    echo "Password update completed.";
 } else {
     echo "No users found.";
 }
