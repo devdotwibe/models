d7105d1eb487b8620518a08d1ee212567607b30a
ArjunlalDotwibe <arjund@twibe@3>
2025-07-15 20:18:50 +0530

    up

--- password.php
+++ password.php
@@ -1,4 +1,4 @@
-<?php
+<?php /*
 include('includes/config.php');
 
 $sql = "SELECT id, password FROM model_user";
@@ -8,7 +8,7 @@ if ($result && mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_assoc($result)) {
         $id = $row['id'];
         $plain_password = $row['password'];
-        
+
         $hashed_password = password_hash($plain_password, PASSWORD_DEFAULT);
         $escaped_password = mysqli_real_escape_string($con, $hashed_password);
 
@@ -21,4 +21,4 @@ if ($result && mysqli_num_rows($result) > 0) {
 } else {
     echo "No users found.";
 }
-?>
+*/?>
