8be83bf7a6e68e9ac195cbc58b4a6684784ed6bc
ArjunlalDotwibe <arjund@twibe@3>
2025-07-21 19:12:09 +0530

    up

--- live-stream/stream.php
+++ live-stream/stream.php
@@ -1488,7 +1488,7 @@ $session_id = $_GET['unique_model_id'];
 
                         if ($('#active_user-' + item.id).length === 0) {
                                 var html = `
-                                     <div class="conversation-item active" onclick="selectConversation('alex')" active_user-${item.id}>
+                                     <div class="conversation-item active" onclick="selectConversation('alex')" id="active_user-${item.id}">
                                         <div class="user-avatar vip">
                                             <img src="${item.image_url}" alt="user image">
                                             <div class="online-indicator public" id="alexIndicator"></div>
