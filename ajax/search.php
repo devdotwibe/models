07611707cf1b80ac27c4e9e6da17bcac628c401d
ArjunlalDotwibe <arjund@twibe@3>
2025-07-22 14:43:04 +0530

    up

--- ajax/search.php
+++ ajax/search.php
@@ -1,22 +1,22 @@
-<?php session_start();
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+session_start();
 include('includes/config.php');
-include('includes/helper.php');
-
-
+include('includes/helper.php'); // Make sure this contains get_data()
 
 if (isset($_POST['search']) && !empty($_POST['search'])) {
     $search = $_POST['search'];
 
-    $userDetails = search_user('model_user', ['name' => $search, 'username' => $search], false);
+    $userDetails = get_data('model_user', ['name' => $search, 'username' => $search], false);
 
     if (!empty($userDetails)) {
-
         foreach ($userDetails as $user) {
-
             echo "<div>{$user['name']} ({$user['username']})</div>";
         }
     } else {
-
         echo "<div>No results found.</div>";
     }
 }
+?>
