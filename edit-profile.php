baf3a8d28d05e0b3ae91fc49251cea2ce09801b8
ArjunlalDotwibe <arjund@twibe@3>
2025-07-25 20:50:48 +0530

    up

--- edit-profile.php
+++ edit-profile.php
@@ -1904,7 +1904,9 @@ $lang_list = modal_language_list();
           if ($totalAmount > 0) {
               $percentageThisMonth = ($totalAmount_month / $totalAmount) * 100;
           }
-          
+
+          $percentageThisMonth = number_format($percentageThisMonth, 2);
+
         ?>
       <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
         <div class="glass-effect p-4 rounded-lg">
