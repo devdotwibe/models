0f8e10af8036a40d409b20100eb2c2f503035202
ArjunlalDotwibe <arjund@twibe@3>
2025-07-28 20:45:36 +0530

    up

--- includes/helper.php
+++ includes/helper.php
@@ -94,7 +94,7 @@ function filterFollowedModelIdsByPrivacy($con, $followed_model_unique_ids, $user
 
 		if ($privacy['country_enable'] && $row['name'] =='STACIA') {
 
-			prin_r($target_country);
+			print_r($target_country);
 
 			die();
 
