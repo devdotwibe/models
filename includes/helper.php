eab61607666273134c049dac7470b71bb6e1d2d1
ArjunlalDotwibe <arjund@twibe@3>
2025-07-28 20:38:56 +0530

    up

--- includes/helper.php
+++ includes/helper.php
@@ -94,9 +94,9 @@ function filterFollowedModelIdsByPrivacy($con, $followed_model_unique_ids, $user
 
 		if ($privacy['country_enable']) {
 
-            if ($target_country != $current_user_country) {
-                $allow = false;
-            }
+            // if ($target_country != $current_user_country) {
+            //     $allow = false;
+            // }
         }
 
 
