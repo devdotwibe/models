7eda0278e35cf068ee5ab61349c4cef3ca1e0f36
ArjunlalDotwibe <arjund@twibe@3>
2025-08-01 21:27:39 +0530

    up

--- optimized_services.php
+++ optimized_services.php
@@ -133,7 +133,7 @@ else{
 
                             $formattedDate = date('M d', strtotime($meeting_date)); 
                         ?>
-                        <span class="service-meta text-orange-400"><?php echo $booking_for ?> • <?php echo $formattedDate ?>, <?php echo $item['meeting_time'] ?></span>
+                        <span class="service-meta text-orange-400"><?php echo $item['booking_for'] ?> • <?php echo $formattedDate ?>, <?php echo $item['meeting_time'] ?></span>
                     </div>
                     </div>
 
@@ -165,7 +165,13 @@ else{
                        }
                     ?>
                     <div class="flex items-center gap-3 mt-2">
-                    <span class="amount-display">$<?php echo $token_amount ?></span>
+
+                    <?php if(!empty($token_amount)) { ?>
+
+                        <span class="amount-display">$<?php echo $token_amount ?></span>
+
+                    <?php }?>
+
                     <span class="status-badge badge-pending">Pending</span>
                     </div>
                 </div>
