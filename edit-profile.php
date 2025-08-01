76b9e9f66f8b0051f3be575303c9949d4761f2f1
ArjunlalDotwibe <arjund@twibe@3>
2025-08-01 14:38:25 +0530

    up

--- edit-profile.php
+++ edit-profile.php
@@ -1846,7 +1846,7 @@ $lang_list = modal_language_list();
     <div class="flex justify-center mt-8">
 			<input type="hidden" name="use_id" value="<?php echo $_SESSION["log_user_id"]; ?>">
 			<input type="hidden" name="model_unique_id" value="<?php echo $userDetails['unique_id']; ?>">
-      <button class="btn-primary text-lg px-8 py-4" type="submit" name="service_submit" onclick="saveCreatorSettings()">Save Creator Settings</button>
+      <button class="btn-primary text-lg px-8 py-4" type="button" name="service_submit" onclick="saveCreatorSettings()">Save Creator Settings</button>
     </div>
 	</form>
   </div>
