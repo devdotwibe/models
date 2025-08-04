5471f7b807f3d98d826ecc7fb936da8b5d27e6d3
ArjunlalDotwibe <arjund@twibe@3>
2025-08-04 11:28:12 +0530

    up

--- optimized_services.php
+++ optimized_services.php
@@ -384,6 +384,25 @@ else{
 
           </div>
       </div>
+
+    <div class="modal-overlay" id="success_modal">
+      <div class="modal">
+          <div class="modal-header">
+              <h2 class="modal-title">Success</h2>
+              <button class="close-modal" id="closeTipModal" type="button" onclick="CloseModal('success_modal')">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                      <line x1="18" y1="6" x2="6" y2="18"></line>
+                      <line x1="6" y1="6" x2="18" y2="18"></line>
+                  </svg>
+              </button>
+          </div>
+          <div class="modal-body" id="modal_success_message">
+              
+
+              <button class="btn btn-primary" type="button" onclick="CloseModal('success_modal')">Close</button>
+          </div>
+      </div>
+  </div>
 	
    <?php include('includes/footer.php'); ?>
 
@@ -425,6 +444,14 @@ else{
                     $('#modal_success_message').prepend(`<p class="success-text">${response.message}</p>`);
 
                     $('#conform_modal').removeClass('active');
+
+                    $('#success_modal').addClass('active');
+
+                    setTimeout(function()
+                    {
+                          $('#success_modal').removeClass('active');
+                          
+                    },1000);
                 }
             },
 
