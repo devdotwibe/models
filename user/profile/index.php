d35a3afb492c62a06104a1a98b4530a32b9cdb05
ArjunlalDotwibe <arjund@twibe@3>
2025-08-05 21:37:15 +0530

    up

--- user/profile/index.php
+++ user/profile/index.php
@@ -170,8 +170,7 @@ if(!empty($userDetails['profile_pic'])){
                 }
             }
 
-            print_r($followed_user_ids);
-            die();
+           
     }
 
     if (!empty($followed_user_ids) && count($followed_user_ids) > 0 ) {
