df9e300963d453eecabec1cb08db764944233566
ArjunlalDotwibe <arjund@twibe@3>
2025-08-05 21:01:18 +0530

    up

--- user/profile/index.php
+++ user/profile/index.php
@@ -173,7 +173,12 @@ if(!empty($userDetails['profile_pic'])){
             }
     }
 
-    if (!empty($followed_user_ids) && count($followed_user_ids) > 1 ) {
+    if (!empty($followed_user_ids) && count($followed_user_ids) > 0 ) {
+
+
+      print_r($followed_user_ids);
+
+      die();
 
         $placeholders = implode(',', array_fill(0, count($followed_user_ids), '?'));
         $types = str_repeat('i', count($followed_user_ids));
