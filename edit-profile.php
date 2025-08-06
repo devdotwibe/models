378ad691b78806f0e4f99735a19734e84b60e364
ArjunlalDotwibe <arjund@twibe@3>
2025-08-06 14:40:16 +0530

    up

--- edit-profile.php
+++ edit-profile.php
@@ -379,8 +379,11 @@ $lang_list = modal_language_list();
                     }
 
                     ?>
+
+                  <div id="temporary-preview-container" style="display: none;"></div>
+
                   <div id="modalimage_gallery" class="text-center dropzone"></div>
-                  
+
                   </ul>
 
                   <div class="preview" style="display:none;">
@@ -3557,7 +3560,7 @@ $lang_list = modal_language_list();
       acceptedFiles: ".jpg,.png,.jpeg,.JPG,.JPEG,.PNG",
       addRemoveLinks: true,
       parallelUploads: 5,
-      previewsContainer: '.visualizacao',
+      previewsContainer: "#temporary-preview-container",
       previewTemplate: jQuery('.preview').html(),
       dictDefaultMessage: '<svg class="w-8 h-8 mx-auto text-white/50 mb-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg><p class="text-xs text-white/60">Add Photo</p>',
       init: function() {
