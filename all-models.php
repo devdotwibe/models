ecdb355bcb5d07c0f238cf0c3deb3dcaeb11c124
ArjunlalDotwibe <arjund@twibe@3>
2025-08-11 18:29:17 +0530

    up

--- all-models.php
+++ all-models.php
@@ -55,6 +55,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&family=Playfair+Display:wght@400;500;600;700;800&display=swap" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
     
+    <link rel='stylesheet' href='<?= SITEURL ?>assets/css/profile.css?v=<?= time() ?>' type='text/css' media='all' />
     <!-- Structured Data -->
     <script type="application/ld+json">
     {
@@ -74,7 +75,7 @@
 
 
 
-<body class="premimum-model1">
+<body class="premimum-model1 enhanced5">
 
     <?php include('includes/header.php'); ?>
 
