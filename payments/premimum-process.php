718adc8fb3a62b4919d3ca5b117ecaf05b3d191a
ArjunlalDotwibe <arjund@twibe@3>
2025-08-12 19:30:37 +0530

    up

--- payments/premimum-process.php
+++ payments/premimum-process.php
@@ -51,13 +51,7 @@ if($userDetails){
                         $amount = $_SESSION["pay_amount"];
                         $plan_status = $_SESSION["plan_status"];
                         $plan_type = $_SESSION["plan_type"];
-
-                        echo "Amount: " . htmlspecialchars($amount) . "<br>";
-                        echo "Plan Status: " . htmlspecialchars($plan_status) . "<br>";
-                        echo "Plan Type: " . htmlspecialchars($plan_type) . "<br>";
-
-                        die();
-
+                        
                         $insertedPremiumUserId = DB::table('premium_users')->insertGetId([
                             'user_id'    => $userDetails['id'],
                             'plan_type'  => $plan_type,
