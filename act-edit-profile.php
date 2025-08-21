757bee2a4346793b4bd036c492be751e1ca0efdf
coderdotwibe <coder@dotwibe.com>
2025-08-21 11:03:49 +0530

    ch

--- act-edit-profile.php
+++ act-edit-profile.php
@@ -109,7 +109,7 @@ if ($_POST['submit_name']){
 		//echo '<script>alert("'.$error.'");</script>';
 		echo json_encode(['status' => 'Error in saving']);
 	}
-	echo json_encode(['status' => 'success']); exit;
+	
 	//Physical attributes save
 		$arr_proof = array();
 		$arr_proof = array('height_type','weight_type','weight','hair_color','eye_color','ethnicity','body_type','dress_size','bust_size','waist_size','cup_size');
