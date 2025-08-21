bfe030e2f1213da60e1e7c96bc9910a3318fa90d
ArjunlalDotwibe <arjund@twibe@3>
2025-08-21 14:27:28 +0530

    up

--- edit-profile.php
+++ edit-profile.php
@@ -4065,7 +4065,7 @@ $extra_details = DB::queryFirstRow("SELECT * FROM model_extra_details WHERE uniq
 
           function AddjustImage() {
 
-            $('#temporary-preview-container').append(hiddenmedia);
+            $('#temporary-preview-container').find('.dz-processing').append(hiddenInput);
 
             const content = $('#temporary-preview-container').html();
 
