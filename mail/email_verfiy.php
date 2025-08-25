ae5f7ab8b6014c2da1ec9d24ba6890daedfd1e51
ArjunlalDotwibe <arjund@twibe@3>
2025-08-25 19:45:16 +0530

    up

--- mail/email_verfiy.php
+++ mail/email_verfiy.php
@@ -18,8 +18,8 @@
       <tr>
         <?php
 
-            $path = '/assets/images/logo-live.jpg';
-            
+            $path = '../assets/images/logo-live.jpg';
+
             $type = pathinfo($path, PATHINFO_EXTENSION);
             $data = file_get_contents($path);
             $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
