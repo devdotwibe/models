2d743313432a869820b1f018cb53a9d9e458307e
ArjunlalDotwibe <arjund@twibe@3>
2025-08-25 18:36:43 +0530

    up

--- act-register.php
+++ act-register.php
@@ -162,6 +162,10 @@ if (isset($_POST['vfb-submit'])) {
    
     $password_hashed = password_hash($password, PASSWORD_DEFAULT);
 
+
+    dd('test');
+
+    die();
  	// $que = "INSERT INTO `model_user` (`unique_id`, `name`, `username`, `email`, `password`, `country`,`gender`,`as_a_model`,`user_bio`,`services`) 
 	// VALUES ('".$uni_id."', '".$name."', '".$user_name."', '".$email."', '".$password_hashed."', '".$country."', '".$gender."', '".$as_a_model."', '".$user_bio."', '".$services."')";
 
