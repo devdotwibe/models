c22a1b0ff839ed4eaf59be508fc4e594dbdd3f87
ArjunlalDotwibe <arjund@twibe@3>
2025-08-25 19:12:24 +0530

    up

--- verify.php
+++ verify.php
@@ -7,7 +7,7 @@
     if(isset($_GET['email']) && isset($_GET['token'])){
 
         $email = mysqli_real_escape_string($con, $_GET['email']);
-        
+
         $token = mysqli_real_escape_string($con, $_GET['token']);
 
         $sql = "SELECT * FROM model_user WHERE email='$email' AND verify_token='$token' AND verified=0 LIMIT 1";
@@ -16,7 +16,9 @@
         if(mysqli_num_rows($result) == 1){
             $update = "UPDATE model_user SET verified=1 WHERE email='$email'";
             if(mysqli_query($con, $update)){
-                echo "<h2>Email Verified Successfully. You can now <a href='" . SITEURL . "/login.php'>Login</a></h2>";
+
+                echo "<script>window.location='" . SITEURL . "/login.php?email=verified' </script>";
+
             }
         } else {
             echo "<h2>Invalid or Expired Verification Link.</h2>";
