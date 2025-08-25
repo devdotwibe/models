560a5ba7f3766c9f28cf9fbcaf083d34d98e1908
ArjunlalDotwibe <arjund@twibe@3>
2025-08-25 19:52:21 +0530

    up

--- index.php
+++ index.php
@@ -675,27 +675,6 @@ include('includes/helper.php');
 </div>
 
 
-    <div class="modal-overlay" id="success_modal">
-        <div class="modal">
-            <div class="modal-header">
-                <h2 class="modal-title">Registration Successful</h2>
-                <button class="close-modal" type="button" onclick="CloseModal('success_modal')">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                        <line x1="18" y1="6" x2="6" y2="18"></line>
-                        <line x1="6" y1="6" x2="18" y2="18"></line>
-                    </svg>
-                </button>
-            </div>
-            <div class="modal-body" id="modal_success_message">
-
-                <p>Your registration was successful! You can now login to your account.</p>
-
-                <a class="btn btn-primary" href="login.php">login</a>
-
-            </div>
-        </div>
-    </div>
-
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 
     <script>
