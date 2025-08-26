b4e92d59e3455d3c567f82446f67c4ab4d5cc857
ArjunlalDotwibe <arjund@twibe@3>
2025-08-26 15:20:44 +0530

    up

--- includes/side-bar.php
+++ includes/side-bar.php
@@ -96,16 +96,7 @@ if (isset($_SESSION['log_user_id'])) {
       Wallet
     </div>
 	
-	<?php if (isset($_SESSION['log_user_id']) && $as_a_model == 'No') { ?>
-	
-	<div class="menu-item" onclick="navigateTo('my-purchase.php')">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" />
-      </svg>
-      Purchase
-    </div>
-	
-	<?php } ?>
+
 
     <div class="menu-item" onclick="navigateTo('optimized_services.php')">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -114,21 +105,7 @@ if (isset($_SESSION['log_user_id'])) {
       Services
     </div>
 
-	<?php if($as_a_model == 'Yes'){ ?>
-    <div class="menu-item" onclick="navigateTo('advertisement/list.php')">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z" />
-      </svg>
-      Advertisement
-    </div>
-	<?php } else{ ?>
-	<div class="menu-item" onclick="navigateTo('advertisements')">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z" />
-      </svg>
-      Advertisement
-    </div>
-	<?php } ?>
+
 
     <div class="menu-item" onclick="navigateTo('boost-advertisement/index.php')">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
