e9c07a9f0bc52d985d6b9e1d53cbee456ce39cd3
ArjunlalDotwibe <arjund@twibe@3>
2025-08-26 17:43:47 +0530

    up

--- includes/side-bar.php
+++ includes/side-bar.php
@@ -91,12 +91,12 @@ if (isset($_SESSION['log_user_id'])) {
       My Activity
     </div>
 
-    <div class="menu-item" onclick="navigateTo('#')">
+    <!-- <div class="menu-item" onclick="navigateTo('#')">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1" />
       </svg>
       Add Social Links
-    </div>
+    </div> -->
 
     <div class="menu-item" onclick="navigateTo('wallet.php')">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
