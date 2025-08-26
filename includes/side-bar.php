724b9ae08f7eb64b8345a089d2bd0d4999f6fe35
ArjunlalDotwibe <arjund@twibe@3>
2025-08-26 15:05:12 +0530

    up

--- includes/side-bar.php
+++ includes/side-bar.php
@@ -84,6 +84,13 @@ if (isset($_SESSION['log_user_id'])) {
     </div>
 
     <div class="menu-item" onclick="navigateTo('single-profile.php?m_unique_id=<?php echo $unique_id; ?>')">
+
+    <?php
+    
+     echo "test";
+
+  die();
+  ?>
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
       </svg>
@@ -111,12 +118,7 @@ if (isset($_SESSION['log_user_id'])) {
       Wallet
     </div>
 	
-	<?php if (isset($_SESSION['log_user_id']) && $as_a_model == 'No') { 
-    
-    
-  echo "test";
-
-  die();?>
+	<?php if (isset($_SESSION['log_user_id']) && $as_a_model == 'No') { ?>
 	
 	<div class="menu-item" onclick="navigateTo('my-purchase.php')">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
