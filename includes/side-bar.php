4a6ff909c4f8107acfde9c35935ddb57db936c1b
ArjunlalDotwibe <arjund@twibe@3>
2025-08-26 15:58:13 +0530

    up

--- includes/side-bar.php
+++ includes/side-bar.php
@@ -1,25 +1,4 @@
 <?php 
-if (isset($_SESSION['log_user_id'])) {
-	$log_user_id = $_SESSION['log_user_id'];
-	$get_modal_user = DB::query('select * from model_user where id='.$log_user_id);
-	if(!empty($get_modal_user[0]['username'])){
-		$modalname = ucfirst($get_modal_user[0]['username']);
-	}else{
-		$modalname = ucfirst($get_modal_user[0]['name']);
-	}
-	$as_a_model = $get_modal_user[0]['as_a_model'];
-	$unique_id = $get_modal_user[0]['unique_id'];
-}else{ 
-	$log_user_id = 0; 
-	$get_modal_user = array(); 
-	$as_a_model = '';
-	$unique_id = '';
-}
 
-  $notify_count = 0;
-
-  $$followes_count = 0;
-
-  $following_count = 0;
   
 ?>
