da3820ba3a596c9239ce4b2c93c35fb550564a1f
ArjunlalDotwibe <arjund@twibe@3>
2025-08-26 15:47:18 +0530

    up

--- includes/side-bar.php
+++ includes/side-bar.php
@@ -1,27 +1,26 @@
-<?php
-$log_user_id = 0;
-$modalname   = '';
-$as_a_model  = '';
-$unique_id   = '';
-
-if (!empty($_SESSION['log_user_id'])) {
-    $log_user_id = (int) $_SESSION['log_user_id'];
-    $get_modal_user = DB::query("SELECT * FROM model_user WHERE id = %i", $log_user_id);
-
-    if ($get_modal_user) {
-        $user       = $get_modal_user[0];
-        $modalname  = ucfirst(!empty($user['username']) ? $user['username'] : $user['name']);
-        $as_a_model = $user['as_a_model'];
-        $unique_id  = $user['unique_id'];
-    }
+<?php 
+if (isset($_SESSION['log_user_id'])) {
+	$log_user_id = $_SESSION['log_user_id'];
+	$get_modal_user = DB::query('select * from model_user where id='.$log_user_id);
+	if(!empty($get_modal_user[0]['username'])){
+		$modalname = ucfirst($get_modal_user[0]['username']);
+	}else{
+		$modalname = ucfirst($get_modal_user[0]['name']);
+	}
+	$as_a_model = $get_modal_user[0]['as_a_model'];
+	$unique_id = $get_modal_user[0]['unique_id'];
+}else{ 
+	$log_user_id = 0; 
+	$get_modal_user = array(); 
+	$as_a_model = '';
+	$unique_id = '';
 }
 
-  $notify_count = 0;
+  $notify_count = NotificationCount($log_user_id);
 
-  $$followes_count = 0;
+  $followes_count = getModelFollowersCount($unique_id);
 
-  $following_count = 0;
-
+  $following_count = getModelFolloweringCount($unique_id);
 
 ?>
  <div class="sidebar-overlay" id="sidebarOverlay"></div>
@@ -123,7 +122,7 @@ if (!empty($_SESSION['log_user_id'])) {
     </div>
 
 	<?php if($as_a_model == 'Yes'){ ?>
-    <div class="menu-item" onclick="navigateTo('advertisement/list.php')">
+    <div class="menu-item" onclick="navigateTo('advertisements')">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z" />
       </svg>
