9c809f40f7bf5c1e198d1ddadd216a1d45756c4c
ArjunlalDotwibe <arjund@twibe@3>
2025-08-26 15:29:19 +0530

    up

--- advertisement/list.php
+++ advertisement/list.php
@@ -47,12 +47,6 @@ if($as_a_model != 'Yes'){
 <body class="min-h-screen text-white profile-advts advt-page  socialwall-page">
    
    <?php  include('../includes/side-bar.php'); ?>
-
-     <?php 
-    echo "test"; 
-    die(); 
-    ?>
-    
 	<?php  include('../includes/profile_header_index.php'); 
 
     ?>  
