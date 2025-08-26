9af43d0c9b46bb0ead03428bc6218186ba4cdb74
ArjunlalDotwibe <arjund@twibe@3>
2025-08-26 15:48:25 +0530

    up

--- includes/side-bar.php
+++ includes/side-bar.php
@@ -27,7 +27,8 @@ if (isset($_SESSION['log_user_id'])) {
 
   <div class="sidebar-menu" id="sidebarMenu">
 
-
+  <?php echo "test" ; die(); ?>
+  
     <div class="p-6 flex flex-col items-center" onclick="window.location.href='<?= SITEURL ?>/user/profile/index.php'">
 
       <img src="<?= SITEURL . 'ajax/noimage.php?image=' . $get_modal_user[0]['profile_pic']; ?>" alt="Profile" class="w-20 h-20 rounded-full">
