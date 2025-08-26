80e5c7c24355954bdf9f38a0fe08b0c4b2997123
ArjunlalDotwibe <arjund@twibe@3>
2025-08-26 16:04:29 +0530

    up

--- includes/side-bar.php
+++ includes/side-bar.php
@@ -5,13 +5,13 @@ if (isset($_SESSION['log_user_id'])) {
 
   $get_modal_user = get_data('model_user',array('id'=>$_SESSION["log_user_id"]),true);
 
-	if(!empty($get_modal_user['username'])){
-		$modalname = ucfirst($get_modal_user['username']);
-	}else{
-		$modalname = ucfirst($get_modal_user['name']);
-	}
-	$as_a_model = $get_modal_user['as_a_model'];
-	$unique_id = $get_modal_user['unique_id'];
+	// if(!empty($get_modal_user['username'])){
+	// 	$modalname = ucfirst($get_modal_user['username']);
+	// }else{
+	// 	$modalname = ucfirst($get_modal_user['name']);
+	// }
+	// $as_a_model = $get_modal_user['as_a_model'];
+	// $unique_id = $get_modal_user['unique_id'];
 
 
 }else{ 
