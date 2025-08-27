9a2268ad7e9875f7a8f16b79a53d3ca668f31135
ArjunlalDotwibe <arjund@twibe@3>
2025-08-27 18:48:48 +0530

    up

--- ajax/delete_adv_uploads.php
+++ ajax/delete_adv_uploads.php
@@ -3,28 +3,56 @@ session_start();
 include('../includes/config.php');
 include('../includes/helper.php');
 $output = array();
+
 $upl_name = $_GET['upl_name'];
 $adv_id = $_GET['adv_id'];
 $adv_type = $_GET['adv_type'];
-if(!empty($upl_name) && !empty($adv_id)){
-	$form_data = DB::queryFirstRow("select image,additionalimages,video from banners where id='" . $adv_id . "' and user_id='" . $_SESSION['log_user_id'] . "' ");
-	if($adv_type == 'video'){
-		if(!empty($form_data['video']) ){ 
-			$video = explode('|',$form_data['video']);
-			$additional_vd = '';
-			foreach($video as $add_vd){	
-				if($add_vd != $upl_name){
-					$additional_vd .= $add_vd.'|';
+
+	if(!empty($upl_name) && !empty($adv_id)){
+
+		$form_data = DB::queryFirstRow("select image,additionalimages,video from banners where id='" . $adv_id . "' and user_id='" . $_SESSION['log_user_id'] . "' ");
+
+		if($adv_type == 'video'){
+
+			if(!empty($form_data['video']) ){
+
+				$video = explode('|',$form_data['video']);
+
+				$additional_vd = '';
+
+				foreach($video as $add_vd){	
+
+					if($add_vd != $upl_name){
+
+						$additional_vd .= $add_vd.'|';
+					}
 				}
+				if(!empty($additional_vd)){
+
+					$joe_id = DB::update('banners', array('video' => rtrim($additional_vd, "|")), "id=%s", $adv_id);
+				}
+
+			 	$output['status'] = 'success';
+            	$output['type']   = $additional_vd;
+
 			}
-			if(!empty($additional_vd)){
-				$joe_id = DB::update('banners', array('video' => rtrim($additional_vd, "|")), "id=%s", $adv_id);
+			}else if($adv_type == 'image'){
+				
 			}
-		}
-	}else if($adv_type == 'image'){
+			else
+			{
+				$output['status']  = 'error';
+				$output['message'] = 'Invalid type or no data found';
+			}
 		
 	}
-	
-}else $output['status']= 'error';
+
+	else {
+
+		   $output['status']  = 'error';
+           $output['message'] = 'Banner not found';
+
+	}
+
 echo json_encode($output);
 ?>
