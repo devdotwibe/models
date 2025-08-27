744b778abea331f7e5a740cc2e3183b833b4d0c9
ArjunlalDotwibe <arjund@twibe@3>
2025-08-27 21:04:56 +0530

    up

--- payments/premimum-process.php
+++ payments/premimum-process.php
@@ -83,7 +83,7 @@ if($userDetails){
 
 						$_SESSION["payment_done"] = "Payment Successfully Completed";
 
-						echo '<script>window.loaction.redirectback()</script>';
+						echo "<script>window.location='wallet.php'</script>";
 
 					// } else {
 					// 	echo "Error in wallet update: " . mysqli_error($con);
