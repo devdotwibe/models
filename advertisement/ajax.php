cdbd8901d861e060bdfeeaca25d3ca4fc973bb52
ArjunlalDotwibe <arjund@twibe@3>
2025-08-28 20:39:14 +0530

    up

--- advertisement/ajax.php
+++ advertisement/ajax.php
@@ -36,7 +36,7 @@ if(isset($_SESSION['log_user_id'])){
 
 
 	if (!empty($category)) {
-		$where_clause = " WHERE category = %s ";
+		$where_clause = " WHERE tb.category = %s ";
 		$params[] = $category;
 	}
 
