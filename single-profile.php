bf93560ebf5a17302306fa586d75dfb6e67b2d7b
ArjunlalDotwibe <arjund@twibe@3>
2025-08-29 16:37:51 +0530

    up

--- single-profile.php
+++ single-profile.php
@@ -883,7 +883,11 @@ body .owl-carousel .owl-nav.disabled {
                                     <div class="action-dropdown" id="moreActions">
 
                                         <button class="btn-secondary px-3 py-2 rounded-full text-white" id="moreActionsBtn">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="1"></circle><circle cx="19" cy="12" r="1"></circle><circle cx="5" cy="12" r="1"></circle></svg>
+
+                                        
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="1"></circle><circle cx="19" cy="12" r="1"></circle><circle cx="5" cy="12" r="1"></circle></svg>
+
+                                            <span id="mobile-explore">Explore</span>
                                         </button>
 
 
