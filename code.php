102b3b0cbf3f1e2b206ff17508f35299260f98d5
ArjunlalDotwibe <arjund@twibe@3>
2025-08-29 17:03:51 +0530

    up

--- code.php
+++ code.php
@@ -1,18 +1,18 @@
 <?php 
+$rootPath = $_SERVER['DOCUMENT_ROOT']; 
+$imagePath = $rootPath . '/' . ltrim('assets/images/logo-live.jpg', '/');
 
+$base64 = '';
 
-	$rootPath = $_SERVER['DOCUMENT_ROOT']; 
-
-        $imagePath = $rootPath . '/' . ltrim('assets/images/logo-live.jpg', '/');
-
-        $base64 = '';
-
-        $imageData = @file_get_contents($imagePath);
-        if ($imageData !== false) {
-            $type = pathinfo($imagePath, PATHINFO_EXTENSION);
-            $base64 = 'data:image/' . $type . ';base64,' . base64_encode($imageData);
-        }
-
-
-        echo $base64 ;
+$imageData = @file_get_contents($imagePath);
+if ($imageData !== false) {
+    $type = pathinfo($imagePath, PATHINFO_EXTENSION);
+    $base64 = 'data:image/' . $type . ';base64,' . base64_encode($imageData);
+}
 ?>
+
+<?php if (!empty($base64)): ?>
+    <img src="<?= $base64 ?>" alt="Logo">
+<?php else: ?>
+    <p>Image not found.</p>
+<?php endif; ?>
