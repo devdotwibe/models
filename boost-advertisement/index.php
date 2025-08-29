76b941446fc78da2a13aaa0114b448624f2a05c2
ArjunlalDotwibe <arjund@twibe@3>
2025-08-29 18:57:27 +0530

    up

--- boost-advertisement/index.php
+++ boost-advertisement/index.php
@@ -799,6 +799,26 @@ else{
     </div>
 
 
+    <div class="modal-overlay" id="error_modal">
+      <div class="modal">
+          <div class="modal-header">
+              <h2 class="modal-title">Success</h2>
+              <button class="close-modal" id="closeTipModal" type="button" onclick="CloseModal('error_modal')">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                      <line x1="18" y1="6" x2="6" y2="18"></line>
+                      <line x1="6" y1="6" x2="18" y2="18"></line>
+                  </svg>
+              </button>
+          </div>
+          <div class="modal-body" id="modal_error_message">
+              
+
+              <button class="btn btn-primary" type="button" onclick="CloseModal('error_modal')">Close</button>
+          </div>
+      </div>
+    </div>
+
+
 <?php include('../includes/footer.php'); ?>
 
 
@@ -818,6 +838,14 @@ else{
         $('#conform_modal').addClass('active');
     }
 
+    function showErrorModal(message) {
+
+        $('#modal_error_message').html("<p style='color:red;'>" + message + "</p>" +
+            '<button class="btn btn-primary" type="button" onclick="CloseModal(\'error_modal\')">Close</button>');
+        $('#error_modal .modal-title').text("Error");
+        $('#error_modal').addClass('active');
+    }
+
     function SubmitLaunch() {
 
         var target_audience = $('#target_audience').val();
@@ -828,8 +856,33 @@ else{
         var total_amount = $('#total_amount').val();
         var expected_views_range = $('#expected_views_range').val();
         var reached_views_range = $('#reached_views_range').val();
-        var user_unique_id = $('#user_unique_id').val();
         var plan_type = $('#plan_type').val();
+
+           var requiredFields = {
+                "Target Audience": target_audience,
+                "Location": location,
+                "Age Range": age_range,
+                "Budget": budget,
+                "Duration": duration,
+                "Total Amount": total_amount,
+                "Expected Views": expected_views_range,
+                "Reached Views": reached_views_range,
+                "Plan Type": plan_type
+            };
+
+
+
+        for (var field in requiredFields) {
+
+            if (!requiredFields[field] || requiredFields[field].trim() === "") {
+                showErrorModal(field + " is required!");
+                return false;
+            }
+        }
+
+     
+        var user_unique_id = $('#user_unique_id').val();
+
         var ad_id = $('#ad_id').val();
 
         $.ajax({
