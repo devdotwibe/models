9c208d7419ec8f184bc1e6a8faa7218d8556a030
ArjunlalDotwibe <arjund@twibe@3>
2025-08-29 21:22:39 +0530

    up

--- advertisements/ajax.php
+++ advertisements/ajax.php
@@ -22,11 +22,11 @@ if(!$page){
 $list_data = $perPage;
 
 
-$name = $_GET['q'];
-if($name){
-    $url .= 'q='.$name.'&';
-    $where_clause .= " (lower(name) like '%".strtolower($name)."%' ) and";
-}
+// $name = $_GET['q'];
+// if($name){
+//     $url .= 'q='.$name.'&';
+//     $where_clause .= " (lower(name) like '%".strtolower($name)."%' ) and";
+// }
 
 $sort_by = ' order by tb.is_paid desc, tb.id desc';
 $sort_column = $_GET['sort_column'];
@@ -44,31 +44,6 @@ $sort_type = $_GET['sort_type'];
     }
 
 
-    // $filter_follower_ids = [];
-
-    // if (!empty($boosted_user_ids)) {
-
-    //     $in_clause = implode(',', array_fill(0, count($boosted_user_ids), '?'));
-
-    //     $types_follower = str_repeat('s', count($boosted_user_ids));
-
-    //     $followQuery = "SELECT id FROM model_user WHERE unique_id IN ($in_clause)";
-
-    //     $stmt = $con->prepare($followQuery);
-
-    //     if (!$stmt) {
-    //         die("Prepare failed: " . $con->error);
-    //     }
-    //     $stmt->bind_param($types_follower, ...$boosted_user_ids);
-    //     $stmt->execute();
-    //     $result = $stmt->get_result();
-
-      
-    //     while ($row = $result->fetch_assoc()) {
-    //         $filter_follower_ids[] = $row['id'];
-    //     }
-
-    // }
 
     if (!empty($boosted_ad_ids)) {
 
@@ -87,21 +62,6 @@ $sort_type = $_GET['sort_type'];
         $sort_by="";
     }
 
-
-    // $allowedOrders = ['mu.id ASC', 'mu.id DESC', 'mu.age ASC', 'mu.age DESC'];
-
-    // $order = in_array($_GET['order'] ?? 'mu.id DESC', $allowedOrders) ?  'mu.id DESC' : 'mu.id DESC';
-
-// $stringQuery = " select tb.*,mu.age from banners tb join model_user mu on mu.id=tb.user_id ";//where status
-
-
-// $stringQuery = "
-//     SELECT tb.*, mu.age 
-//     FROM banners tb 
-//     JOIN model_user mu ON mu.id = tb.user_id 
-//     WHERE mu.verified = '1' AND mu.id IN ($basicList)
-//     $order
-// ";
 
     $basic_filed_users = GetUsersWithBasicFilled();
 
@@ -131,29 +91,26 @@ $sort_type = $_GET['sort_type'];
 
 
 if($_GET['country']){
+
     $where_clause .= " tb.country='".$_GET['country']."' and";
 }
 if($_GET['category']){
+
     $where_clause .= " tb.category='".$_GET['category']."' and";
 }
-if($_GET['service']){
-    $where_clause .= " tb.service='".$_GET['service']."' and";
-}
-if($_GET['state']){
-    $where_clause .= " tb.state='".$_GET['state']."' and";
-}
-if($_GET['city']){
-    $where_clause .= " tb.city='".$_GET['city']."' and";
-}
+
 
 $limited = " limit $offset, ".$perPage;
+
 $where_clause = rtrim($where_clause,'and');
+
 if($where_clause){
-    //echo $stringQuery." where ".$where_clause." ".$sort_by.$limited;
+
+  
     $all_data = DB::query($stringQuery." where ".$where_clause." ".$sort_by.$limited );
-    $total = $output['total'] = DB::numRows($stringQuery." and ".$where_clause );
+    $total = $output['total'] = DB::numRows($stringQuery." where ".$where_clause );
 	
-	$output['total_page_all'] = $stringQuery." and ".$where_clause;
+	$output['total_page_all'] = $stringQuery." where ".$where_clause;
 }
 else{
     $all_data = DB::query($stringQuery." ".$sort_by.$limited );
