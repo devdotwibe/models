b013fcf891b1fed1dfd51ac8989f3dafc1505ac9
ArjunlalDotwibe <arjund@twibe@3>
2025-09-02 20:35:45 +0530

    up

--- includes/side-bar.php
+++ includes/side-bar.php
@@ -157,7 +157,7 @@ if (isset($_SESSION['log_user_id'])) {
       Boost Advertisement
     </div> -->
 
-    <div class="menu-item" onclick="navigateTo('#')">
+    <div class="menu-item" onclick="navigateTo('chat-app.php')">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
       </svg>
