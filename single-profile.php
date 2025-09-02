05e09ac6cad588082052bbc4fe65e687f15271aa
ArjunlalDotwibe <arjund@twibe@3>
2025-09-02 16:41:25 +0530

    up

--- single-profile.php
+++ single-profile.php
@@ -1018,9 +1018,10 @@ body .owl-carousel .owl-nav.disabled {
                                                 <div class="action-item" id="block-profile" bis_skin_checked="1">
 
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                                        <circle cx="12" cy="12" r="10"></circle>
-                                                        <line x1="12" y1="16" x2="12" y2="12"></line>
-                                                        <line x1="12" y1="8" x2="12.01" y2="8"></line>
+                                                        <rect x="3" y="8" width="18" height="4" rx="1"></rect>
+                                                        <path d="M12 8v13"></path>
+                                                        <path d="M19 12v7a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-7"></path>
+                                                        <path d="M7.5 8a2.5 2.5 0 0 1 0-5A4.8 8 0 0 1 12 8a4.8 8 0 0 1 4.5-5 2.5 2.5 0 0 1 0 5"></path>
                                                     </svg>
 
                                                     Block Profile
