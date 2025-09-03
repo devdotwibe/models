9e8639bf7a15c2d4be13eee438d350dd4395d889
ArjunlalDotwibe <arjund@twibe@3>
2025-09-03 20:12:34 +0530

    up

--- all-models.php
+++ all-models.php
@@ -971,6 +971,10 @@ include('includes/helper.php');
                     }
                 }
 
+                echo $sqls;
+
+                die();
+
                 $resultd = mysqli_query($con, $sqls);
 
                 if (mysqli_num_rows($resultd) > 0) {
