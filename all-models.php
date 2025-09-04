c5d35a8d4970010eeeef6fdce2a208d800b5f3e2
ArjunlalDotwibe <arjund@twibe@3>
2025-09-04 14:45:35 +0530

    up

--- all-models.php
+++ all-models.php
@@ -948,10 +948,6 @@ include('includes/helper.php');
 
                             } 
 
-                            echo $idPrivacy ;
-
-                            die();
-
                             // $sqls_count = "SELECT COUNT(*) AS total FROM model_user WHERE as_a_model = 'Yes' ".$where; 
 
                             $sqls_count = "SELECT COUNT(*) AS total FROM model_user mu  WHERE mu.verified = '1' " . $where;
@@ -969,10 +965,6 @@ include('includes/helper.php');
 
                                 $idPrivacy = "'" . implode("','", $privacy_user_ids) . "'";
                             } 
-
-                              echo $idPrivacy ;
-
-                            die();
 
                             $idList = implode(',', $onlineUserIds);
 
