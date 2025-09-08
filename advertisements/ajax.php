16e9d4f92a1c187c7427db290c8494ae8f3197fc
ArjunlalDotwibe <arjund@twibe@3>
2025-09-08 11:57:46 +0530

    up

--- advertisements/ajax.php
+++ advertisements/ajax.php
@@ -99,8 +99,6 @@ $limited = " LIMIT $offset, " . (int)$perPage;
 $finalQuery = $stringQuery . $orderBy . $limited;
 
 
-print_r($finalQuery); die();
-
 $all_data   = DB::query($finalQuery, ...$params);
 
 print_r($all_data); die();
