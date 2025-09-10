21c835216aa78e786cf71214fb7935d4878f5a1d
ArjunlalDotwibe <arjund@twibe@3>
2025-09-10 21:43:12 +0530

    up

--- ajax/adv_upload.php
+++ ajax/adv_upload.php
@@ -1,15 +1,30 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['uploaded_file'])) {
 	
+	$target_dir_profile = "../uploads/banners/"; 
+
 			$totalFiles_v = count($_FILES['uploaded_file']['name']);
 			$additional_vd = '';
-			$target_dir_profile = "../uploads/banners/";
+	
 				for ($i = 0; $i < $totalFiles_v; $i++) {
-					$target_file1 = $target_dir_profile . basename($_FILES["uploaded_file"]["name"][$i]);
-					$target_profile = basename($_FILES["uploaded_file"]["name"][$i]);
+
+					// $target_file1 = $target_dir_profile . basename($_FILES["uploaded_file"]["name"][$i]);
+
+					// $target_profile = basename($_FILES["uploaded_file"]["name"][$i]);
+
+					$ext = pathinfo($_FILES["uploaded_file"]["name"][$i], PATHINFO_EXTENSION);
+
+					$new_filename = uniqid("video_", true) . "." . $ext;
+
+					$target_file1 = $target_dir_profile . $new_filename;
+					$target_profile = "uploads/banners/" . $new_filename; 
+
+
 					if (move_uploaded_file($_FILES["uploaded_file"]["tmp_name"][$i], $target_file1)) {
+						
 						$additional_vd .= $target_profile.'|';
 					}
+
 				}
 				if(!empty($additional_vd)){ 
 					//$joe_id = DB::update('banners', array('video' => rtrim($additional_vd, "|")), "id=%s", $id);
