8b54ffb0872703cea0982df35f96d06d4e03207e
ArjunlalDotwibe <arjund@twibe@3>
2025-09-10 21:45:45 +0530

    up

--- ajax/adv_upload.php
+++ ajax/adv_upload.php
@@ -17,7 +17,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['uploaded_file'])) {
 					$new_filename = uniqid("video_", true) . "." . $ext;
 
 					$target_file1 = $target_dir_profile . $new_filename;
-					$target_profile = "uploads/banners/" . $new_filename; 
+					$target_profile = $new_filename; 
 
 
 					if (move_uploaded_file($_FILES["uploaded_file"]["tmp_name"][$i], $target_file1)) {
