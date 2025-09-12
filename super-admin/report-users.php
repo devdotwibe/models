8ba52e9eca31205418cb37c950f0ce1694d84921
ArjunlalDotwibe <arjund@twibe@3>
2025-09-12 16:37:03 +0530

    up

--- super-admin/report-users.php
+++ super-admin/report-users.php
@@ -1,7 +1,7 @@
 <?php
   session_start();
   include('includes/config.php');
-  
+  include('../includes/helper.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -83,9 +83,9 @@
 
                                 $attachment = $rowesdw['attachment'];
 
-                                // $reported_by_detail = get_data('model_user',array('id'=>$reported_by_id),true);
+                                $reported_by_detail = get_data('model_user',array('id'=>$reported_by_id),true);
 
-                                // $repoted_user_detail = get_data('model_user',array('id'=>$repoted_user_id),true);
+                                $repoted_user_detail = get_data('model_user',array('id'=>$repoted_user_id),true);
 
                                 $repoted_date = $rowesdw['created_at'];
 
@@ -93,12 +93,10 @@
 
                                 $imageUrl = "";
 
-                                // if (checkImageExists($attachment)) {
+                                if (checkImageExists($attachment)) {
 
-                                //     $imageUrl = SITEURL . $attachment;
-                                // }
-
-                                //  $imageUrl = SITEURL . $attachment;
+                                    $imageUrl = SITEURL . $attachment;
+                                }
 
                         ?>
 
@@ -107,14 +105,16 @@
                             <?php echo $count; ?>
                           </td>
                           <td>
+                            <?php echo $reported_by_detail['name']; ?>
                           </td>
                           <td>
+                            <?php echo $repoted_user_detail['name']; ?>
                           </td>
                           <td>
                             <?php echo $rowesdw['description']; ?>
                           </td>
                           <td>
-                                <img src="<?php echo $imageUrl; ?>" >
+                                <img src="<?php echo $imageUrl ?>" >
                           </td>
                           <td>
                             <?php echo $f_report_date; ?>
