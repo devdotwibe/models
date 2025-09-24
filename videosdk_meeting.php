813979160af79fdbe22edbd53dee155fffe94cd1
jerindotwibe <jerindotwibe@outlook.com>
2025-09-24 11:28:50 +0530

    wewewe

--- videosdk_meeting.php
+++ videosdk_meeting.php
@@ -85,7 +85,7 @@ $VIDEOSDK_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiJkOWUwNWQ2
       const meeting = VideoSDK.initMeeting({
         meetingId: meetingId,
         name: "Guest User",
-        apiKey: TOKEN,
+        token: TOKEN,  // ✅ FIX: use token instead of apiKey
         containerId: "meeting-container",
         micEnabled: true,
         webcamEnabled: true,
